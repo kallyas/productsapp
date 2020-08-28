8a358e542f1f4f5dd4915da27438cf613743fc03
Kally <kallyasmedia@gmail.com>
2020-08-28 09:01:58 +0000

    Add Node engine

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 
-let mongoDB = process.env.MONGODB_URI || null;
+let mongoDB =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://admin:RYGvAr1ine0kZmVf@api.4wjwu.mongodb.net/core?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
