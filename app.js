9f2d660041e26d9a1320ffdb09e5b1b8aac7628c
Kally <kallyasmedia@gmail.com>
2020-08-28 08:54:46 +0000

    updated heroku configs

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 
 const product = require("./routes/product.route");
 
+require("dotenv").config();
+
 const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-let dev_db_url =
-  "mongodb+srv://admin:RYGvAr1ine0kZmVf@api.4wjwu.mongodb.net/core?retryWrites=true&w=majority";
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+let mongoDB = process.env.MONGODB_URI || null;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
@@ -18,9 +19,14 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/", (req, res) => {
+  res.send(
+    "<h1>Welcome to products app! Please visit /products end point to fetch data</h1>"
+  );
+});
 app.use("/products", product);
 
-const PORT = 3333;
+const PORT = process.env.PORT || 5200;
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
